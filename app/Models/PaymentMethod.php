6122f3a6ea75483e8304f7deb82e668830209909
NeilGreenShoes <neilkarlgrencio@gmail.com>
2026-07-05 20:12:53 +0800

    composer update

--- app/Models/PaymentMethod.php
+++ app/Models/PaymentMethod.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class PaymentMethod extends Model
 {
-    //
+    protected $table = 'payment_methods';
+    protected $primaryKey = 'paymentMethodID';
+    protected $keyType = 'int';
+    public $incrementing = true;
+    public $timestamps = true;
+
+    protected $fillable = [
+        'methodName',
+    ];
+
+    public function payments()
+    {
+        return $this->hasMany(Payments::class, 'paymentMethodID', 'paymentMethodID');
+    }
 }
