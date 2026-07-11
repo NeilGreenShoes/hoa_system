7838024b28f2f18e4a85535b9d8b8eb02bf72cc1
NeilGreenShoes <neilkarlgrencio@gmail.com>
2026-07-11 18:58:20 +0800

    update sendotp

--- app/Mail/sendOtpMail.php
+++ app/Mail/sendOtpMail.php
@@ -8,7 +8,7 @@ use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class sendOtpMail extends Mailable
+class SendOtpMail extends Mailable
 {
     use Queueable, SerializesModels;
 
